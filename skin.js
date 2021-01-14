a36b2737dfc833fe739c7a451e061a1e9cf9fd29
stationdrei <74349125+stationdrei@users.noreply.github.com>
2021-01-14 17:28:47 +0100

    asd

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.10/18007
 // Filename: silhouette_cardboard.ggsk
-// Generated 2021-01-14T17:25:15
+// Generated 2021-01-14T17:28:43
 
 function pano2vrSkin(player,base) {
 	player.addVariable('ht_ani', 2, false);
